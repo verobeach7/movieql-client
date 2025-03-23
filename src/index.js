4e7d02420ee0aad76b50e6f9cebbcfc6403c113d
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-24 00:09:12 +0900

    Movie GraphQL Client: ApolloProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import client from "./client";
+import { ApolloProvider } from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// ApolloProvider로 감싸주면 React Components, Router, Screen을 연결해주게 됨
+// 즉, Provider는 애플리케이션 안의 모두가 이 client에 접근할 수 있게 해주는 것
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,3 +1,37 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 export default function Movies() {
-  return <div>This is a list of movies.</div>;
+  const [movies, setMovies] = useState([]);
+  // ApolloProvider를 통해 client.js에서 생성해 놓은 백엔드와 연결됨
+  const client = useApolloClient();
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then((results) => setMovies(results.data.allMovies));
+  }, [client]);
+  return (
+    <ul>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
+
+/* 
+ useApolloClient를 모든 파일에서 Provider를 통해서 접근하는 것은 반복적인 작업을 계속 해야 하기 때문에 매우 성가심.
+ - client.query, GraphQL Query를 반복적으로 작업
+ - 결과를 받아와서 state 안에 저장
+ - state를 계속해서 만들고 state의 이름을 다 기억해야 됨
+ - REST API를 이용하여 fetch해오고 받은 데이터를 다시 처리해야 된다는 점이 꼭 닮아 있음
+*/
