f07d7382fe98a0ed70550b1e2fd2cea8a65c186e
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-24 20:04:07 +0900

    Movie GraphQL Client: useQuery Hook by Apollo Client

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,37 +1,35 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  # allMovies라고 해도 되고 getMovies라고 해도 potato라고 해도 됨. 아예 쓰지 않아도 상관 없음
+  query getMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-  // ApolloProvider를 통해 client.js에서 생성해 놓은 백엔드와 연결됨
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((results) => setMovies(results.data.allMovies));
-  }, [client]);
+  // result에 무엇이 반환되어 들어오는지 확인
+  // const result = useQuery(ALL_MOVIES);
+  // console.log(result);
+
+  // useQuery Hook은 Declarative code(선언형 코드)를 사용하게 해 줌
+  // 선언형 코드는 원하는 것을 설명하기 위한 코드만 적는 것을 말함
+  // 반면에 Imperative code(명령형 코드)는 모든 단계의 코드를 적는 것을 말함
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
     <ul>
-      {movies.map((movie) => (
+      {data.allMovies.map((movie) => (
         <li key={movie.id}>{movie.title}</li>
       ))}
     </ul>
   );
 }
-
-/* 
- useApolloClient를 모든 파일에서 Provider를 통해서 접근하는 것은 반복적인 작업을 계속 해야 하기 때문에 매우 성가심.
- - client.query, GraphQL Query를 반복적으로 작업
- - 결과를 받아와서 state 안에 저장
- - state를 계속해서 만들고 state의 이름을 다 기억해야 됨
- - REST API를 이용하여 fetch해오고 받은 데이터를 다시 처리해야 된다는 점이 꼭 닮아 있음
-*/
