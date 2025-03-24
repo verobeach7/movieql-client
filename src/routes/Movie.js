77e372821833999992f25b2c73fcb85ca95f8823
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-24 21:23:08 +0900

    Movie GraphQL Client: Apollo Dev Tools

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -8,12 +8,16 @@ const GET_MOVIE = gql`
     movie(id: $movieId) {
       id
       title
+      # allMovies에서 이미 각 movie의 id와 title을 불러와 캐시해 놓았음
+      # 나중에 GET_MOVIE 쿼리에 새로운 data(field)가 추가되면 기존 캐시에 새로운 데이터만 추가함
+      small_cover_image
     }
   }
 `;
 
 export default function Movie() {
   const { id } = useParams();
+  // 쿼리 호출 시는 GET_MOVIE로 접근
   const { data, loading } = useQuery(GET_MOVIE, {
     // 변수 전달
     variables: {
@@ -27,5 +31,6 @@ export default function Movie() {
   if (!data) {
     return <h1>Undefined</h1>;
   }
+  // 쿼리로 반환된 data는 gql 문법으로 작성된 쿼리 내의 이름(movie)을 사용
   return <div>{data.movie.title}</div>;
 }
