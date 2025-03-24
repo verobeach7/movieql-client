ccb2911aa2b59ee717be24e891c0df08e72d35b9
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-03-24 20:55:48 +0900

    Movie GraphQL Client: useQuery Variables

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  # getMovie는 적어도 되고 안 적어도 됨
+  # argument gql 문법으로 삽입
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details.</div>;
+  const { id } = useParams();
+  const { data, loading } = useQuery(GET_MOVIE, {
+    // 변수 전달
+    variables: {
+      movieId: id,
+    },
+  });
+  console.log(data, loading);
+  if (loading) {
+    return <h1>Fetching movie...</h1>;
+  }
+  if (!data) {
+    return <h1>Undefined</h1>;
+  }
+  return <div>{data.movie.title}</div>;
 }
--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const ALL_MOVIES = gql`
   # allMovies라고 해도 되고 getMovies라고 해도 potato라고 해도 됨. 아예 쓰지 않아도 상관 없음
@@ -28,7 +29,9 @@ export default function Movies() {
   return (
     <ul>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   );
